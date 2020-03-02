debcfcd8e0a89360ab9faa4d9674c64d2d451304
wwf <865475679@qq.com>
2020-03-02 11:29:04 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -5,44 +5,48 @@ function $$(id) {
   return document.querySelectorAll(id);
 }
 
-const load = $('.loading')
-const nav = $('.nav')
-const wa = nav.querySelectorAll('a')
-const data = $('#data')
-const skill = $('#skill')
-const work = $('#work')
+// setTimeout(() => {
+//   load.classList.remove('active');
+// }, 3000)
 
-setTimeout(()=>{
-  load.classList.remove('active');
-  
-},3000)
-
-let move = (element, time = 400) => {
-  let speed = Math.abs(window.pageYOffset - element) * 10 / time
-  if (element > window.pageYOffset) {
-    let down = setInterval(() => {
-      document.documentElement.scrollTop > element
-        ? clearInterval(down)
-        : (document.documentElement.scrollTop = window.pageYOffset + speed)
-    }, 5);
+const topBar = $('.topBar')
+window.onscroll = function () {
+  if (window.scrollY > 0) {
+    topBar.classList.add('sticky')
   } else {
-    let up = setInterval(() => {
-      document.documentElement.scrollTop <= element
-        ? clearInterval(up)
-        : (document.documentElement.scrollTop = window.pageYOffset - speed)
-    }, 5);
+    topBar.classList.remove('sticky')
   }
 }
 
-for (let i = 0; i < wa.length; i++) {
-  wa[i].onclick = () => {
-    switch (i) {
-      case 0: move(data.offsetTop);
-        break;
-      case 1: move(skill.offsetTop);
-        break;
-      default: move(work.offsetTop);
+const topA = $$('.menu ul li a ')
+function animate(time) {
+  requestAnimationFrame(animate);
+  TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+for (let i = 0; i < topA.length; i++) {
+  topA[i].onclick = function (x) {
+    x.preventDefault()
+    // let a = x.currentTarget
+    // let href = a.getAttribute('href')
+    // let element = $(href)
+    // let top = element.offsetTop
+    let top = $(x.currentTarget.getAttribute('href')).offsetTop
+    let currentTop = window.scrollY
+    let targetTop = top - 80
+    let s = targetTop - currentTop
+    var coords = { y: currentTop }; // 起始位置
+    var t = Math.abs((s / 100) * 300); // 时间
+    if (t > 500) {
+      t = 500;
     }
-  }
+    var tween = new TWEEN.Tween(coords) // 起始位置
+      .to({ y: targetTop }, t) // 结束位置 和 时间
+      .easing(TWEEN.Easing.Cubic.InOut) // 缓动类型
+      .onUpdate(function () {
+        // coords.y 已经变了
+        window.scrollTo(0, coords.y); // 如何更新界面
+      })
+      .start(); // 开始缓动
+  };
 }
-
